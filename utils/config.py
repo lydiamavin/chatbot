6372c959bbd1695163c3a7d9cb18d6096d379628
lydiamavin <lydiaavin@gmail.com>
2025-08-08 20:07:52 +0530

    IMP:config updated

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_ENV = os.getenv("PINECONE_ENV")
+PINECONE_ENV = "us-east-1"
 PINECONE_INDEX_NAME = "rag-sports-index"
 
 EMBEDDING_MODEL_NAME = "hkunlp/instructor-xl"
@@ -14,5 +14,3 @@ EMBEDDING_MODEL_CACHE = "./model_cache"
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
 PDF_DIRECTORY = "data"
-
-HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
